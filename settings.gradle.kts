6480499f9eaffbeeafd4a328b026f43405436c7b
mehdizareei <mahdi.zareei@miare.ir>
2026-06-03 14:02:37 +0330

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,67 @@
+rootProject.name = "KMPStructure"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+
+        // if your project did not build comment these mavens
+        // ─── Mirror های ایرانی ───────────────────────────────────────
+        maven { url = uri("https://maven.myket.ir") }
+        maven { url = uri("https://maven.devneeds.ir") }
+        maven { url = uri("https://gradle.iranrepo.ir") }
+        maven { url = uri("https://gradle.jamko.ir") }
+        maven { url = uri("https://en-mirror.ir") }
+        maven { url = uri("https://archive.ito.gov.ir/gradle/maven-plugin/") }
+
+        // ─── Mirror های چین (Aliyun) ─────────────────────────────────
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+
+        // if your project did not build comment these mavens
+        // ─── Mirror های ایرانی ───────────────────────────────────────
+        maven { url = uri("https://maven.myket.ir") }
+        maven { url = uri("https://maven.devneeds.ir") }
+        maven { url = uri("https://gradle.iranrepo.ir") }
+        maven { url = uri("https://gradle.jamko.ir") }
+        maven { url = uri("https://en-mirror.ir") }
+        maven { url = uri("https://archive.ito.gov.ir/gradle/maven-plugin/") }
+
+        // ─── Mirror های چین (Aliyun) ─────────────────────────────────
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+include(":androidApp")
+include(":desktopApp")
+include(":shared")
+include(":webApp")
